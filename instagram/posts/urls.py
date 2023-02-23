27804ad147ebb60b853484f48e0692778293c703
Yash Mittal <102669147+yashm02@users.noreply.github.com>
2023-02-23 14:54:28 +0530

    Delete instagram directory

--- instagram/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns=[
-    path('create/',views.post_create,name="create"),
-    path('feed/',views.feed,name="feed"),
-    path('like',views.like_post,name="like"),
-    path('delete/<int:id>/',views.delete,name="delete")
-]
